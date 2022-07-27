8915e9c421f7c160975df0f36dceb19886ed20de
eduuh <edwinkamaumuraya0@outlook.com>
2022-07-27 20:03:33 +0300

    Added a powershell setupscript.

---
+++ windowsetup.ps1
@@ -0,0 +1,15 @@
+scoop install neovim
+scoop bucket add main
+scoop install ripgrep
+scoop install fzf
+scoop bucket add nerd-fonts
+scoop install FiraCode-NF
+
+scoop install gcc
+scoop install python310
+pip install pynvim
+scoop install fd
+scoop install Cascadia-Code
+
+rm -r C:\Users\edwinmuraya\AppData\Local\
+cp -r .config\nvim  C:\Users\edwinmuraya\AppData\Local\
