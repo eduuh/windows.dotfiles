f614e0ccba78659a0701aa2222e4d4fa06b5ba07
edwinmuraya <edwinmuraya@microsoft.com>
2022-08-09 21:53:29 +0300

    disabled fixers for this

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -108,19 +108,8 @@ require('vscode').setup({
 
 
 vim.cmd([[
-   let g:ale_fixers = {
-    \   'typescript': ['prettier', 'eslint'],
-    \   'javascript': ['prettier', 'eslint'],
-    \   'jsx': ['prettier', 'eslint'],
-    \}
-
     let g:ale_linters = {}
     let g:ale_linters.typescript = ['eslint', 'tsserver']
-
-    let g:ale_typescript_prettier_use_local_config = 1
-
-    let g:ale_fix_on_save = 1
-
+    let g:ale_typescript_prettier_use_local_config = 0
     let g:ale_linters_explicit =  1
-
 ]])
